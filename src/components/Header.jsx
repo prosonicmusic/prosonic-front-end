d79d337711f5bf93515df6030fbe112b5a0d7e07
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-06 01:37:56 +0430

    products container added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,15 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 
 const Header = () => {
   return (
     <header className="header header__home">
-      <div className="scroll">
-        <Navbar />
-      </div>
+
 
       <div className="hero">
-        <h1>Listen, Pickup,  Order</h1>
+        <h1>Listen, Pick up,  Order</h1>
         <p>Sound Like a Pro With Prosonic</p>
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
     </header>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,234 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
+import cover1 from '../img/cover1.jpg'
+import playIcon from '../icons/play-icon-1.svg';
+import logo1 from '../img/cubase_logo.png';
 
 function HomePage() {
   return (
     <>
       <Header/>
       <Layout>
-        <h1>Home</h1>
+        
+        <div className='homePage'>
+          <section className='container'>
+
+            <div className='container__header-section'>
+                <h2>Premium Prosonic produced tracks</h2>
+                <div className='container__browse'>
+                  <a href="#" className='container__btn'>Browse more tracks</a>
+                </div>
+            </div>
+
+            <div className='container__grid-5'>
+
+
+                <div className='col'>
+                  <div className='beatItem hoverOnBeatItem'>
+                    <div className='beatItem__top'>
+                      <div className='cover'>
+                        <img src={cover1} alt="cover" />
+                        <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                      </div>
+                      <ul className='beatItem__labels'>
+                        
+                        <li className='Premium'> PREMIUM </li>
+                      </ul>
+                    </div>
+
+                    <div className='beatItem__bottomWrapper bottom-part'>
+                      <div>
+                        <div className='beatItem__nameAndUsername'>
+                          <a href="#" className='name'>Trailer Tension</a><br />
+                          <a href="#" className='username'>Prosonic</a>
+                        </div>
+                        <div className='beatItem__lineData'>
+                          <span className='price'> 500,000 T</span>
+
+                          <div className='daw'>
+                              <img src={logo1} alt="Cubase" />
+                          </div>
+                        </div>
+                        <div className='beatItem__buttons'>
+                          <a href="#" className='info'> More Info </a>
+                          <button className='set-bg'> Add to cart </button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className='col'>
+                  <div className='beatItem hoverOnBeatItem'>
+                    <div className='beatItem__top'>
+                      <div className='cover'>
+                        <img src={cover1} alt="cover" />
+                        <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                      </div>
+                      <ul className='beatItem__labels'>
+                        
+                        <li className='Premium'> PREMIUM </li>
+                      </ul>
+                    </div>
+
+                    <div className='beatItem__bottomWrapper bottom-part'>
+                      <div>
+                        <div className='beatItem__nameAndUsername'>
+                          <a href="#" className='name'>Trailer Tension</a><br />
+                          <a href="#" className='username'>Prosonic</a>
+                        </div>
+                        <div className='beatItem__lineData'>
+                          <span className='price'> 500,000 T</span>
+
+                          <div className='daw'>
+                              <img src={logo1} alt="Cubase" />
+                          </div>
+                        </div>
+                        <div className='beatItem__buttons'>
+                          <a href="#" className='info'> More Info </a>
+                          <button className='set-bg'> Add to cart </button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className='col'>
+                  <div className='beatItem hoverOnBeatItem'>
+                    <div className='beatItem__top'>
+                      <div className='cover'>
+                        <img src={cover1} alt="cover" />
+                        <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                      </div>
+                      <ul className='beatItem__labels'>
+                        
+                        <li className='Premium'> PREMIUM </li>
+                      </ul>
+                    </div>
+
+                    <div className='beatItem__bottomWrapper bottom-part'>
+                      <div>
+                        <div className='beatItem__nameAndUsername'>
+                          <a href="#" className='name'>Trailer Tension</a><br />
+                          <a href="#" className='username'>Prosonic</a>
+                        </div>
+                        <div className='beatItem__lineData'>
+                          <span className='price'> 500,000 T</span>
+
+                          <div className='daw'>
+                              <img src={logo1} alt="Cubase" />
+                          </div>
+                        </div>
+                        <div className='beatItem__buttons'>
+                          <a href="#" className='info'> More Info </a>
+                          <button className='set-bg'> Add to cart </button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className='col'>
+                  <div className='beatItem hoverOnBeatItem'>
+                    <div className='beatItem__top'>
+                      <div className='cover'>
+                        <img src={cover1} alt="cover" />
+                        <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                      </div>
+                      <ul className='beatItem__labels'>
+                        
+                        <li className='Premium'> PREMIUM </li>
+                      </ul>
+                    </div>
+
+                    <div className='beatItem__bottomWrapper bottom-part'>
+                      <div>
+                        <div className='beatItem__nameAndUsername'>
+                          <a href="#" className='name'>Trailer Tension</a><br />
+                          <a href="#" className='username'>Prosonic</a>
+                        </div>
+                        <div className='beatItem__lineData'>
+                          <span className='price'> 500,000 T</span>
+
+                          <div className='daw'>
+                              <img src={logo1} alt="Cubase" />
+                          </div>
+                        </div>
+                        <div className='beatItem__buttons'>
+                          <a href="#" className='info'> More Info </a>
+                          <button className='set-bg'> Add to cart </button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className='col'>
+                  <div className='beatItem hoverOnBeatItem'>
+                    <div className='beatItem__top'>
+                      <div className='cover'>
+                        <img src={cover1} alt="cover" />
+                        <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                      </div>
+                      <ul className='beatItem__labels'>
+                        
+                        <li className='Premium'> PREMIUM </li>
+                      </ul>
+                    </div>
+
+                    <div className='beatItem__bottomWrapper bottom-part'>
+                      <div>
+                        <div className='beatItem__nameAndUsername'>
+                          <a href="#" className='name'>Trailer Tension</a><br />
+                          <a href="#" className='username'>Prosonic</a>
+                        </div>
+                        <div className='beatItem__lineData'>
+                          <span className='price'> 500,000 T</span>
+
+                          <div className='daw'>
+                              <img src={logo1} alt="Cubase" />
+                          </div>
+                        </div>
+                        <div className='beatItem__buttons'>
+                          <a href="#" className='info'> More Info </a>
+                          <button className='set-bg'> Add to cart </button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+               
+
+
+
+                
+            </div>
+          </section>
+        </div>
+
+
       </Layout>
       </>
   )
