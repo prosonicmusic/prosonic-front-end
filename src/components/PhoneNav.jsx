8a99584fe4c7445baf0f99f13047299bb386c98d
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-08 22:53:16 +0430

    burger menu icons added.

--- src/components/PhoneNav.jsx
+++ src/components/PhoneNav.jsx
@@ -1,36 +1,56 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
+import { faBox } from '@fortawesome/free-solid-svg-icons'
+import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
+
 
 function PhoneNav({ status }) {
-  // const [showNav, setShowNav] = useState(false);
-
-  // window.addEventListener('click', setShowNav);
-
   return (
     <div className={`lockWrapper ${status ? "active" : ""}`} id="navbarMenu">
       <div className="wrapper">
         <ul className="menuLink">
-          <li>
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faHome} className="homeIcon"></FontAwesomeIcon>
             <Link to="/">Home</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faFloppyDisk} className="homeIcon"></FontAwesomeIcon>
             <Link to="/tracks">Tracks</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faBox} className="homeIcon"></FontAwesomeIcon>
             <Link to="/packages">Packages</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faScrewdriverWrench} className="homeIcon"></FontAwesomeIcon>
             <Link to="/services">Services</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faUser} className="homeIcon"></FontAwesomeIcon>
             <Link to="/contact">Contact</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faRightToBracket} className="homeIcon"></FontAwesomeIcon>
             <Link to="#">Login</Link>
           </li>
-          <li>
+
+          <li className="menuFlex">
+          <FontAwesomeIcon icon={faUserPlus} className="homeIcon"></FontAwesomeIcon>
             <Link to="/register">Register</Link>
           </li>
+
+          
         </ul>
       </div>
     </div>
