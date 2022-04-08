da28f0ef80dbf32e94a935c552b553a6a4852bc9
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-08 16:09:03 +0430

    contact page (UI done)

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,7 +4,47 @@ import Layout from '../components/Layout'
 function ContactPage() {
   return (
     <Layout>
-      <h1>Contact</h1>
+      <div className="header"></div >
+
+      <div id='contactPage'>
+            <header>
+              <h1>Contact us</h1>
+            </header>
+
+            <div className='contactSlot'>
+              <div id='setting'>
+                <div className='label first'>Name</div>
+                <div className='formInput'>
+                  <input type="text"/>
+                </div>
+
+                <div className='label'>Email</div>
+                <div className='formInput'>
+                  <input type="email"/>
+                </div>
+
+                <div className='label'>Subject</div>
+                <div className='formInput'>
+                  <input type="text"/>
+                </div>
+
+                <div className='label'>Message</div>
+                <div className='formText'>
+                  <textarea style={{height:'150px'}} class></textarea>
+                </div>
+
+                <div className='captcha'>
+                  <div></div>
+                  <div>
+                    <button id='subButton'>
+                      <span className='sub-btn set-bg'>Submit</span>
+                    </button>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
     </Layout>
   )
 }
