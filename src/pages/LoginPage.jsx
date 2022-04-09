77221da89b7f4798132764deda57d34c84cae7fc
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-09 23:21:17 +0430

    movement effect done.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -29,13 +29,24 @@ function LoginPage() {
                 </div>
 
                 <div className={`hide ${move ? "active" : ""}`} id='formBx'>
-                  <div className='form'>
-                    <form className={`signinForm ${move ? "active" : ""}`}>
+                  <div className='form signinForm'>
+                    <form>
                       <h3>Sign In</h3>
                       <input type="text" placeholder='Username'/>
                       <input type="password" placeholder='Password'/>
                       <input type="submit" value="Login"/>
                       <a href="#" className='forgot'>Forgot Password</a>
+                    </form>
+                  </div>
+
+                  <div className='form signupForm'>
+                    <form>
+                      <h3>Sign Up</h3>
+                      <input type="text" placeholder='Username'/>
+                      <input type="email" placeholder='Email Address'/>
+                      <input type="password" placeholder='Password'/>
+                      <input type="password" placeholder='Confirm Password'/>
+                      <input type="submit" value="Register"/>
                     </form>
                   </div>
                 </div>
