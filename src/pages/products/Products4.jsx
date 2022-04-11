c750d55a32511eb48c8e9f2f2b84ef6aad54e11f
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-11 18:36:12 +0430

    products pages created.

---
+++ src/pages/products/Products4.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import cover2 from './../../img/cover2.jpg';
+import playIcon from './../../icons/play-icon-1.svg';
+import logo1 from './../../img/cubase_logo.png';
+import {FontAwesomeIcon} from  '@fortawesome/react-fontawesome';
+import { faChevronRight } from  '@fortawesome/free-solid-svg-icons';
+
+function Products4() {
+  return (
+    <div>Products4</div>
+  )
+}
+
+export default Products4
