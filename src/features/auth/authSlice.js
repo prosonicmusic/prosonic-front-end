50dda5fa3539d568fc5dd812c151ce270d5aa231
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-11 16:15:37 +0430

    authSlice setup

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: ''
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+
+  }
+})
+
+export default authSlice.reducer;
