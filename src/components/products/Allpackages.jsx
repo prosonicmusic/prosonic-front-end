83fe1514cebb07ce3c199e560858f59eb0ffb0f7
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-12 21:47:41 +0430

    Packages page created.

---
+++ src/components/products/Allpackages.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Package from './Package'
+
+function Allpackages() {
+  return (
+    <section className='grid'>
+
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+      <Package/>
+
+    </section>
+  )
+}
+
+export default Allpackages
---
+++ src/components/products/Package.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import pack3 from './../../img/pack3.png';
+import playIcon from './../../icons/play-icon-1.svg';
+import logo1 from './../../img/cubase_logo.png';
+import {FontAwesomeIcon} from  '@fortawesome/react-fontawesome';
+import { faChevronRight } from  '@fortawesome/free-solid-svg-icons';
+
+function Package() {
+  return (
+    <div className='col'>
+              <div className='beatItem hoverOnBeatItem beatItem__MoreInfoForDevices'>
+                <div className='beatItem__top'>
+                  <div className='cover'>
+                    <img src={pack3} alt="cover" />
+                    <span><img src={playIcon} alt="play" className='cover__playIcon'/></span>
+                  </div>
+                  <ul className='beatItem__labels'>
+                      <li className='Premium'> PREMIUM </li>
+                  </ul>
+                  <div className="soldLayer">
+                    <span> SOLD </span>
+                  </div>
+                </div>
+
+                <div className='beatItem__bottomWrapper bottom-part'>
+                  <div className='moreInfoIcon'>
+                      <a href="#"></a>
+                      <FontAwesomeIcon icon={faChevronRight} className="rightIcon"></FontAwesomeIcon>
+                  </div>
+
+                  <div>
+                    <div className='beatItem__nameAndUsername'>
+                      <a href="#" className='name'>Black Dubstep Vol.1</a><br />
+                      <a href="#" className='username'>Drum Kit</a>
+                    </div>
+                    <div className='beatItem__lineData'>
+                      <span className='price'> 300,000 T</span>
+
+                    </div>
+                    <div className='beatItem__buttons'>
+                      <a href="#" className='info'> More Info </a>
+                      <button className='set-bg'> Add to cart </button>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  )
+}
+
+export default Package
