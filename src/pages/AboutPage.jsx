a12c0f270f88ac7f3423f856ea5d5aec11c54140
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-04-15 20:23:22 +0430

    tilt added to the cart

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { FaInstagram, FaTelegramPlane, FaLinkedinIn  } from 'react-icons/fa';
+import { FaGithub, FaTelegramPlane, FaLinkedinIn  } from 'react-icons/fa';
 import pakzad from '../img/pakzad1.png';
+import mahmoud from '../img/mahmoudeslami1.jpg';
+import Tilt from 'react-tilty';
 
 function AboutPage() {
   return (
@@ -10,65 +12,67 @@ function AboutPage() {
         <div className="cardTitle">
           <h1>The Team</h1>
         </div>
-        <section className='team'>    
-          <div className='teamCard'>
-            <div className='cardContent'>
-              <div className='imgBx'>
-                <img src={pakzad} alt="pakzad" />
+        <section className='team'>
+          <Tilt max={10} speed={400} glare={true} maxGlare={0.2}> 
+            <div className='teamCard'>
+              <div className='cardContent'>
+                <div className='imgBx'>
+                  <img src={pakzad} alt="pakzad" />
+                </div>
+                <div className="contentBx">
+                  <h3>Arya Pakzad<br /><span className='chngContent'>Founder, Music Producer</span></h3>
+                </div>
               </div>
-              <div className="contentBx">
-                <h3>Arya Pakzad<br /><span >Founder, Music Producer,<br /> Front-end developer</span></h3>
-              </div>
+
+              <ul className="sci">
+                <li>
+                  <a href="https://github.com/pakzadmusic" target="_blank">
+                    <FaGithub/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://t.me/PakzadMusic" target="_blank">
+                    <FaTelegramPlane/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/paakzad/" target="_blank">
+                    <FaLinkedinIn/>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <ul className="sci">
-              <li>
-                <a href="https://www.instagram.com/paakzadmusic/" target="_blank">
-                  <FaInstagram/>
-                </a>
-              </li>
-              <li>
-                <a href="https://t.me/PakzadMusic" target="_blank">
-                  <FaTelegramPlane/>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/paakzad/" target="_blank">
-                  <FaLinkedinIn/>
-                </a>
-              </li>
-            </ul>
-          </div>
-
-
-          <div className='teamCard'>
-            <div className='cardContent'>
-              <div className='imgBx'>
-                <img src={pakzad} alt="Mahmoud Eslami" />
+          </Tilt> 
+          <Tilt>
+            <div className='teamCard'>
+              <div className='cardContent'>
+                <div className='imgBx'>
+                  <img src={mahmoud} alt="Mahmoud Eslami" />
+                </div>
+                <div className="contentBx">
+                  <h3>Mahmoud Eslami<br /><span>Back-end developer</span></h3>
+                </div>
               </div>
-              <div className="contentBx">
-                <h3>Mahmoud Eslami<br /><span>Back-end developer</span></h3>
-              </div>
+
+              <ul className="sci">
+                <li>
+                  <a href="https://github.com/mahmoud-eslami" target="_blank">
+                    <FaGithub/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://t.me/es_mahmoud" target="_blank">
+                    <FaTelegramPlane/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/mahmoud-eslami/" target="_blank">
+                    <FaLinkedinIn/>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <ul className="sci">
-              <li>
-                <a href="https://www.instagram.com/paakzadmusic/" target="_blank">
-                  <FaInstagram/>
-                </a>
-              </li>
-              <li>
-                <a href="https://t.me/PakzadMusic" target="_blank">
-                  <FaTelegramPlane/>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/paakzad/" target="_blank">
-                  <FaLinkedinIn/>
-                </a>
-              </li>
-            </ul>
-          </div>
+          </Tilt>  
         </section>
       </div>
     </Layout>
