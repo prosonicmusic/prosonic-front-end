da45131c895a76e2f2d459c326711c575a390120
Arya Pakzad <pakzadlive@gmail.com>
2022-08-27 17:08:10 +0430

    api page size changed to 10

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const BASE_URL = "http://localhost:8080";
 
 const getTracks = async () => {
-   const response = await axios.get(`${BASE_URL}/product/get?product_type=Track`);
+   const response = await axios.get(`${BASE_URL}/product/get?product_type=Track&page_size=10`);
    return response.data.results;
 };
 
 const getPackages = async () => {
-   const response = await axios.get(`${BASE_URL}/product/get?product_type=Package`);
+   const response = await axios.get(`${BASE_URL}/product/get?product_type=Package&page_size=10`);
    return response.data.results;
 };
 
