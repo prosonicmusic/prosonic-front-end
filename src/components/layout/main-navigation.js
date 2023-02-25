599e4a3db38e4fd4f84062d48c78955ae71a1c16
pakzad <pakzadlive@gmail.com>
2023-02-25 14:40:32 +0330

    the styles were placed in a variable - making responsive for smaller screens

--- src/components/layout/main-navigation.js
+++ src/components/layout/main-navigation.js
@@ -2,15 +2,24 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { FaShoppingCart } from "react-icons/fa";
+import Burger from "./burger";
 
 function MainNavigation() {
   const listStyles =
-    "py-1 px-2.5 mx-1 rounded text-[15px] transition duration-500 hover:bg-[#5a5a7a81]";
+    "py-[2.5px] px-2.5 mx-1 rounded text-[15px] transition duration-500 hover:bg-[#5a5a7a81] max-[900px]:hidden";
+  const navbarStyles =
+    "flex  items-center justify-between bg-[#141a22cc] w-[100%] h-[60px] fixed top-0 pt-[30px] pb-[30px] transition duration-500 z-50 rounded-b-2xl";
+  const cartStyles =
+    "flex rounded-[2px] bg-[#5a5a7a66] mx-[14px] h-[29px] w-[32px] relative text-center transition duration-500 items-center hover:bg-[#5a5a7a99] z-20 max-[900px]:mr-[40px]";
+  const loginButtonStyles =
+    "py-0.5 px-2.5 bg-[#e91c60e7] rounded-[5px] hover:bg-[#e91c60] transition duration-500 ml-[7px] mr-[0px] max-[900px]:hidden";
+  const cartCount =
+    "absolute top-[-9px] right-[-4px] rounded-full bg-[#e91c60] w-4 h-4 text-[11px] font-black";
 
   return (
     <nav>
-      <div className="flex  items-center justify-between bg-[#141a22cc] w-[100%] h-[60px] fixed top-0 pt-[30px] pb-[30px] transition duration-500 z-50 rounded-b-2xl">
-        <Link href="/" className="w-[160px] relative ml-10">
+      <div className={navbarStyles}>
+        <Link href="/" className="w-[160px] relative ml-10 z-20 max-[900px]:w-[100px] max-[900px]:ml-5">
           <Image
             src="/images/prosonic-icon-white.png"
             alt="Prosonic"
@@ -32,19 +41,16 @@ function MainNavigation() {
           <li className={listStyles}>
             <Link href="">Services</Link>
           </li>
-          <Link
-            href="/cart"
-            className="flex rounded-[2px]  bg-[#5a5a7a66] mx-[14px] h-[29px] w-[32px] relative text-center transition duration-500 items-center hover:bg-[#5a5a7a99]"
-          >
+          <Link href="/cart" className={cartStyles}>
             <FaShoppingCart className="text-[#bcc7d4] h-[16px] w-full" />
-            <span className="absolute top-[-9px] right-[-4px] rounded-full bg-[#e91c60] w-4 h-4 text-[11px] font-black">
-              0
-            </span>
+            <span className={cartCount}>0</span>
           </Link>
 
-          <button className="py-1 px-2.5 bg-[#e91c60e7] rounded-[5px] hover:bg-[#e91c60] transition duration-500 ml-[7px] mr-[0px]">
+          <button className={loginButtonStyles}>
             <Link href="/login">Login</Link>
           </button>
+
+          <Burger />
         </ul>
       </div>
     </nav>
