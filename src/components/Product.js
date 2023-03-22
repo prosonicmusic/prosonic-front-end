bcbc28949856de4fde74b6aa0e20f8df339f8720
pakzad <pakzadlive@gmail.com>
2023-03-22 16:05:08 +0430

    buttons added to product item - working on some stylings

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,28 +7,34 @@ import { FaChevronRight } from "react-icons/fa";
 
 export default function Product({ product }) {
   const strokeStyles =
-    "block relative bg-[#f1f1f1] h-[20px] w-[2px] left-[9.6em] max-md:left-[5.5em] rounded-3xl mx-[1.5px] transition duration-200 stroke";
+    "block bg-[#f1f1f1] h-[20px] w-[2px] left-[9.6em] rounded-3xl mx-[1.5px] transition duration-200 stroke";
 
   return (
-    <div
-      className="md:basis-[20%] basis-[100%] md:max-w-[20%] max-x-[100%] p-[7.5px] z-10"
-      key={product.id}
-    >
-      <div className="bg-[#23252b80] rounded-[10px] transition duration-200 max-md:flex">
+    <div className="basis-full min-[900px]:basis-[20%] min-[900px]:max-w-[20%] max-x-[100%] p-[7.5px] z-10">
+      <div className="bg-[#23252b80] rounded-[10px] transition duration-200 max-[900px]:flex">
         {/* top */}
-        <div className="relative w-full rounded-lg max-md:max-w-[120px]">
+        <div className="relative w-full rounded-lg max-[900px]:max-w-[120px]">
           {product.sold && (
-            <div className="absolute h-full w-full bg-[#23252bd2] top-0 left-0 right-0 z-10 flex items-center justify-center">
+            <div className="absolute h-full w-full bg-[#23252bd2] top-0 left-0 right-0 z-50 flex items-center justify-center">
               <span className="text-[30px] font-bold"> SOLD </span>
             </div>
           )}
 
           {/* image */}
-          <img
-            src={product.thumbnail}
-            alt={product.title}
-            className="max-x-[100%] border-none align-middle rounded-[10px]"
-          />
+          <div>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              className="max-x-[100%] border-none align-middle rounded-[10px]"
+            />
+            {/* wave */}
+            <div className="h-[5px] flex items-center absolute right-2 top-4">
+              <div className={strokeStyles}></div>
+              <div className={strokeStyles}></div>
+              <div className={strokeStyles}></div>
+              <div className={strokeStyles}></div>
+            </div>
+          </div>
 
           {/* play */}
           <div className="cursor-pointer transition-all duration-300 opacity-0 hover:opacity-100 icon">
@@ -41,26 +47,20 @@ export default function Product({ product }) {
           <div className="flex items-start flex-col absolute top-[8px] text-[#bcc7d4] md:text-[11px] text-[9px] font-black invisible">
             <div className={product.tag}>PREMIUM</div>
           </div>
-
-          {/* wave */}
-          <div className="h-[5px] flex items-center absolute top-[17px] wave">
-            <div className={strokeStyles}></div>
-            <div className={strokeStyles}></div>
-            <div className={strokeStyles}></div>
-            <div className={strokeStyles}></div>
-          </div>
         </div>
 
         {/* bottom */}
-        <div className="relative text-[#bcc7d4] text-left max-md:self-center max-md:w-full max-md:h-[120px] max-md:flex max-md:items-center max-md:justify-center">
-          {/* {product.sold ? (
+        <div className="relative text-[#bcc7d4] text-left w-full h-full">
+          {product.sold ? (
             <div></div>
           ) : (
-            <div className="moreInfoIcon">
-              <Link href={`/tracks/${product.id}`} className="info"></Link>
-              <FaChevronRight className="md:hidden" />
-            </div>
-          )} */}
+            <Link
+              href={`/tracks/${product.id}`}
+              className="absolute w-full h-full left-0 top-0 flex items-center justify-center cursor-pointer z-10"
+            >
+              <FaChevronRight className="min-[900px]:hidden absolute right-4 h-5 text-[#dd1f5f]" />
+            </Link>
+          )}
 
           <div className="p-[20px]">
             <span className="text-[#bcc7d4] text-[17px]">{product.title}</span>
@@ -69,10 +69,10 @@ export default function Product({ product }) {
           </div>
 
           <div className="flex items-center justify-start font-bold text-[13px]">
-            <span className="relative px-[12px] bg-[#282b32bb] rounded-[2px] mb-[10px]">
+            <span className="relative px-[12px] bg-[#282b32bb] rounded-[2px] mb-[10px] max-[900px]:ml-5">
               {product.product_price} T
             </span>
-            <div className="px-[3px] py-[3px] ml-[60px] mb-[10px] bg-[#282b32bb] rounded-lg">
+            <div className="px-[3px] py-[3px] min-[900px]:ml-[60px] ml-3 mb-[10px] bg-[#282b32bb] rounded-lg">
               <div className={product.daw}>
                 <img
                   src="images/cubase_logo.png"
@@ -90,6 +90,19 @@ export default function Product({ product }) {
                 />
               </div>
             </div>
+          </div>
+          <div
+            className={`absolute top-0 transition-all duration-200 p-[10px] w-full h-full bg-[#16181b] rounded-[10px] max-[900px]:hidden ${
+              product.sold && "hidden"
+            } opacity-0 hover:opacity-100 z-10`}
+          >
+            <div className="h-[45%] w-full mb-[10px] bg-[#282b32] text-[#bcc7d4] transition-all duration-200 flex items-center justify-center rounded-[10px]">
+              <Link href={`/tracks/${product.id}`}> More Info </Link>
+            </div>
+            <button className="bg-[#dd1f5f] font-semibold h-[45%] w-full text-white flex items-center justify-center rounded-[10px]">
+              {" "}
+              Add to cart{" "}
+            </button>
           </div>
         </div>
       </div>
