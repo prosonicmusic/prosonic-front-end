a5d8885e17e08202a2dd3ee5b57bbeac53a6edf4
pakzad <pakzadlive@gmail.com>
2023-04-19 22:42:46 +0430

    removing custom player functions

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -35,85 +35,9 @@ const reducer = (state, action) => {
 
 export default function PlayerProvider({ children }) {
   const [audio, dispatch] = useReducer(reducer, initialState);
-  // states
-  const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
-
-  const audioPlayer = useRef(null);
-  const progressBar = useRef(); // reference our progress bar
-  const animationRef = useRef(); // reference the animation
-
-  useEffect(() => {
-    const seconds = Math.floor(audioPlayer.current.duration);
-    setDuration(seconds);
-    progressBar.current.max = seconds;
-  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
-
-  useEffect(() => {
-    if (currentTime == duration) {
-      togglePlayPause();
-      timeTravel(0);
-    }
-  }, [currentTime]);
-
-  const calculateTime = (secs) => {
-    const minutes = Math.floor(secs / 60);
-    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const seconds = Math.floor(secs % 60);
-    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${returnedMinutes}:${returnedSeconds}`;
-  };
-
-  const togglePlayPause = () => {
-    dispatch({ type: "PLAY_PAUSE", payload: !audio?.playing });
-
-    if (!audio?.playing) {
-      audioPlayer.current.play();
-      animationRef.current = requestAnimationFrame(whilePlaying);
-    } else {
-      audioPlayer.current.pause();
-      cancelAnimationFrame(animationRef.current);
-    }
-  };
-
-  const whilePlaying = () => {
-    progressBar.current.value = audioPlayer.current.currentTime;
-    changePlayerCurrentTime();
-    animationRef.current = requestAnimationFrame(whilePlaying);
-  };
-
-  const changeRange = () => {
-    audioPlayer.current.currentTime = progressBar.current.value;
-    changePlayerCurrentTime();
-  };
-
-  const changePlayerCurrentTime = () => {
-    progressBar.current.style.setProperty(
-      "--seek-before-width",
-      `${(progressBar.current.value / duration) * 100}%`
-    );
-    setCurrentTime(progressBar.current.value);
-  };
-
-  const timeTravel = (newTime) => {
-    progressBar.current.value = newTime;
-    changeRange();
-  };
-
-  // const backThirty = () => {
-  //   progressBar.current.value = Number(progressBar.current.value - 30);
-  //   changeRange();
-  // };
-
-  // const forwardThirty = () => {
-  //   progressBar.current.value = Number(progressBar.current.value + 30);
-  //   changeRange();
-  // };
 
   return (
-    <PlayerContext.Provider
-      value={{ audio, audioPlayer, progressBar, changeRange, togglePlayPause }}
-    >
+    <PlayerContext.Provider value={audio}>
       <PlayerContextDispatcher.Provider value={dispatch}>
         {children}
       </PlayerContextDispatcher.Provider>
