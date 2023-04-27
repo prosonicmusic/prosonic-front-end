1040e6274c3daa95e43dbdad6e5412a52f161f17
pakzad <pakzadlive@gmail.com>
2023-04-27 16:15:08 +0430

    working on OTP and user signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,10 +10,13 @@ const AuthContext = createContext();
 const AuthContextDispatcher = createContext();
 const cookies = parseCookies();
 
+const baseUrl = "http://localhost:4545";
+
 const initialState = {
   user: null,
   loading: true,
   error: null,
+  otp: false,
 };
 
 const reducer = (state, action) => {
@@ -22,8 +25,10 @@ const reducer = (state, action) => {
       return { error: null, loading: true, user: null };
     case "SIGNIN_SUCCESS":
       return { error: null, loading: false, user: action.payload };
+    case "OTP_SUCCESS":
+      return { ...state, error: null, otp: true };
     case "SIGNIN_REJECT":
-      return { error: action.error, loading: false, user: null };
+      return { error: action.error, loading: false, user: null, otp: false };
     default:
       return { ...state };
   }
@@ -36,10 +41,11 @@ const asyncActionHandlers = {
       dispatch({ type: "SIGNIN_PENDING" });
 
       axios
-        .post("http://localhost:4545/user/register", action.payload)
+        .post(`${baseUrl}/user/register`, action.payload)
         .then(({ data }) => {
           toast.success("You have successfully registered");
           dispatch({ type: "SIGNIN_SUCCESS", payload: data });
+          window.location.href = "/signin";
         })
         .catch((err) => {
           dispatch({ type: "SIGNIN_REJECT", error: err?.response?.data?.detail });
@@ -59,29 +65,31 @@ const asyncActionHandlers = {
       dispatch({ type: "SIGNIN_PENDING" });
 
       axios
-        .post("http://localhost:4545/login", action.payload)
+        .post(`${baseUrl}/login`, action.payload)
         .then(({ data }) => {
-          setCookie(null, "accessToken", data?.access, {
-            maxAge: 30 * 24 * 60 * 60,
-            path: "/",
-            secure: true,
-            sameSite: "strict",
-          });
-          setCookie(null, "refreshToken", data?.refresh, {
-            maxAge: 30 * 24 * 60 * 60,
-            path: "/",
-            secure: true,
-            sameSite: "strict",
-          });
-
-          toast.success("You have successfully logged in");
-          dispatch({ type: "SIGNIN_SUCCESS", payload: data });
-          // Router.push("/");
+          if (data.verified) {
+            setCookie(null, "accessToken", data?.access, {
+              maxAge: 30 * 24 * 60 * 60,
+              path: "/",
+              secure: true,
+              sameSite: "strict",
+            });
+            setCookie(null, "refreshToken", data?.refresh, {
+              maxAge: 30 * 24 * 60 * 60,
+              path: "/",
+              secure: true,
+              sameSite: "strict",
+            });
+            toast.success("You have successfully logged in");
+            dispatch({ type: "SIGNIN_SUCCESS", payload: data });
+            Router.push("/");
+          } else {
+            toast.error("Your email address is not verified");
+          }
         })
         .catch((err) => {
           dispatch({ type: "SIGNIN_REJECT", error: err?.response?.data?.detail });
           toast.error(err?.response?.data?.detail);
-          console.log(err);
         });
     },
 
@@ -91,7 +99,7 @@ const asyncActionHandlers = {
       if (cookies) {
         const refresh = cookies.refreshToken;
         axios
-          .post("http://localhost:4545/token/refresh", { refresh: refresh })
+          .post(`${baseUrl}/token/refresh`, { refresh: refresh })
           .then(({ data }) => {
             console.log("access token updated");
 
@@ -101,8 +109,30 @@ const asyncActionHandlers = {
               secure: true,
               sameSite: "strict",
             });
+          })
+          .catch((err) => {
+            dispatch({ type: "SIGNIN_PENDING" });
+            destroyCookie(null, "accessToken");
+            destroyCookie(null, "refreshToken");
+            window.location.href = "/";
+            toast.error("Please login again");
           });
       }
+    },
+
+  SET_OTP:
+    ({ dispatch }) =>
+    (action) => {
+      axios
+        .post(`${baseUrl}/user/verification/send`, action.payload)
+        .then(({ data }) => {
+          dispatch({ type: "OTP_SUCCESS" });
+          toast.success(data?.message);
+        })
+        .catch((err) => {
+          dispatch({ type: "SIGNIN_REJECT", error: "Something went wrong, Check your email" });
+          toast.error(err?.response?.data?.message);
+        });
     },
 
   SIGNOUT:
@@ -123,7 +153,7 @@ export default function AuthProvider({ children }) {
       if (cookies.accessToken) {
         dispatch({ type: "UPDATE_TOKEN" });
       }
-    }, 10000);
+    }, 860000);
     return () => clearInterval(interval);
   }, [user.loading, cookies]);
 
