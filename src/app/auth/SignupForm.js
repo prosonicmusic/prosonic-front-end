3c6aa651ec17b4852f15848472ef51512ae17ee1
pakzad <pakzadlive@gmail.com>
2023-07-10 17:50:22 +0330

    Using React query for data fetching - auth components and signin page moved to the app route

---
+++ src/app/auth/SignupForm.js
@@ -0,0 +1,269 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import InputComponent from "@/common/FormInput";
+import Loading from "@/common/Loading";
+import { registerOTP } from "@/services/authServices";
+
+const initialSignUpValues = {
+  name: "",
+  username: "",
+  signupEmail: "",
+  phoneNumber: "",
+  signupPassword: "",
+  confirmPassword: "",
+  otp: "",
+  terms: false,
+};
+
+// validation schema
+const signUpvalidationSchema = Yup.object({
+  name: Yup.string()
+    .required("Enter your full name")
+    .min(6, "Your name must contain at least 6 characters"),
+  signupEmail: Yup.string().required("Enter your email").email("The email is invalid"),
+  username: Yup.string()
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .required("Enter a username")
+    .min(4, "Password must contain at least 4 characters"),
+  phoneNumber: Yup.string()
+    .required("Enter your Phone number")
+    .matches(
+      /^\+\d{6,14}$/,
+      "The mobile number must be 15 digits long and have a + sign with the country code"
+    )
+    .nullable(),
+  signupPassword: Yup.string()
+    .required("Enter a Password")
+    .min(8, "Password must contain at least 8 characters")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter")
+    .matches(/[!@#$%^&()_+]/, "At least one special character (!@#$%^&()_+)")
+    .matches(/^\S*$/, "No whitespace"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("signupPassword"), null], "Re-enter the password")
+    .required("The password does not match"),
+  otp: Yup.number()
+    .required("Enter the code sent to the email")
+    .min(5, "Enter the code correctly"),
+  terms: Yup.bool()
+    .required("The terms and conditions must be accepted.")
+    .oneOf([true], "The terms and conditions must be accepted."),
+});
+
+export default function SignupForm({ move, otp, setOtp }) {
+  const [timer, setTimer] = useState(300);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [emailEntered, setEmailEntered] = useState(false);
+  const [showResendButton, setShowResendButton] = useState(false);
+
+  const {
+    data,
+    error,
+    isLoading,
+    mutateAsync: mutateSignupOTP,
+  } = useMutation({ mutationFn: registerOTP });
+
+  const onSubmitSignUp = (values) => {
+    const { name, username, signupEmail, phoneNumber, signupPassword, otp } = values;
+    const signupValues = {
+      username,
+      email: signupEmail,
+      name,
+      password: signupPassword,
+      phone_number: phoneNumber,
+      otp,
+    };
+
+    // dispatch({ type: "SIGNUP", payload: signupValues });
+  };
+
+  const signUpFormik = useFormik({
+    initialValues: initialSignUpValues,
+    onSubmit: onSubmitSignUp,
+    validationSchema: signUpvalidationSchema,
+    validateOnMount: true,
+  });
+
+  useEffect(() => {
+    let interval = null;
+    if (timer > -1) {
+      interval = setInterval(() => {
+        const seconds = timer % 60;
+        const minutes = Math.floor(timer / 60);
+        setSeconds(seconds);
+        setMinutes(minutes);
+        setTimer((timer) => timer - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+      setShowResendButton(true);
+    }
+    return () => clearInterval(interval);
+  }, [timer]);
+
+  useEffect(() => {
+    if (signUpFormik.values.signupEmail) {
+      setEmailEntered(true);
+    } else {
+      setEmailEntered(false);
+    }
+  }, [signUpFormik]);
+
+  const verifyHandler = async () => {
+    setTimer(300);
+    setShowResendButton(false);
+
+    const body = {
+      email: signUpFormik.values?.signupEmail,
+      type: "register",
+    };
+
+    if (signUpFormik.values.signupEmail) {
+      try {
+        const { data } = await mutateSignupOTP(body);
+
+        toast.success("Verification code sent");
+
+        setOtp('signup');
+        setTimer(300);
+        setShowResendButton(false);
+      } catch (error) {
+        if (error?.response?.data?.message == "Check your email") {
+          setOtp('signup');
+          setTimer(300);
+          setShowResendButton(false);
+
+          toast.error(error?.response?.data?.message);
+        } else {
+          setOtp('');
+
+          toast.error(error?.response?.data?.message);
+        }
+      }
+    } else {
+      toast.error("Enter your email");
+    }
+
+    signUpFormik.handleSubmit();
+  };
+
+  return (
+    <section className="delay-[0s]">
+      <form
+        className={`absolute top-0 w-full p-[50px] transition-all duration-[.45s] flex flex-col ${
+          move ? "left-0" : "left-[100%]"
+        }`}
+        onSubmit={signUpFormik.handleSubmit}
+      >
+        <h3 className="text-[1.5em] font-medium mb-6">Sign Up</h3>
+        <div className="formGroup">
+          <InputComponent name="username" formik={signUpFormik} placeholder="username" />
+          <InputComponent name="name" formik={signUpFormik} placeholder="Full Name" />
+          <InputComponent
+            name="signupEmail"
+            formik={signUpFormik}
+            placeholder="Email"
+            type="email"
+          />
+          <InputComponent
+            name="phoneNumber"
+            formik={signUpFormik}
+            placeholder="Phone Number"
+            type="tel"
+          />
+          <InputComponent
+            name="signupPassword"
+            formik={signUpFormik}
+            placeholder="Password"
+            type="password"
+          />
+          <InputComponent
+            name="confirmPassword"
+            formik={signUpFormik}
+            placeholder="Confirm Password"
+            type="password"
+          />
+
+          {/* policy */}
+          {signUpFormik.errors.terms && signUpFormik.touched.terms && (
+            <div className="mb-1 ml-1 text-rose-500 text-left text-xs">
+              {signUpFormik.errors.terms}
+            </div>
+          )}
+          <div className="flex items-center mb-6 ml-1">
+            <label className="text-sm">
+              I accept terms of
+              <Link
+                href="/terms"
+                className="underline transition-all duration-300 hover:text-[#e91c60f8] mx-1"
+              >
+                Privacy Policy
+              </Link>
+            </label>
+            <input
+              className="ml-3 w-5 h-5 border border-white appearance-none rounded cursor-pointer checked:bg-[#1aa2d8d3] checked:rounded-full"
+              type="checkbox"
+              id="terms"
+              name="terms"
+              value={true}
+              onChange={signUpFormik.handleChange}
+              checked={signUpFormik.values.terms}
+            />
+          </div>
+
+          {/* verification code */}
+          {otp && (
+            <div className="flex items-center">
+              <InputComponent
+                name="otp"
+                formik={signUpFormik}
+                placeholder="Verification Code"
+                type="number"
+              />
+
+              {seconds > 0 || minutes > 0 ? (
+                <p className="ml-3">
+                  {minutes < 10 ? `0${minutes}` : minutes}:
+                  {seconds < 10 ? `0${seconds}` : seconds}
+                </p>
+              ) : showResendButton ? (
+                <button className="ml-3" onClick={verifyHandler}>
+                  Resend
+                </button>
+              ) : (
+                <p className="ml-3">Waiting for OTP...</p>
+              )}
+            </div>
+          )}
+
+          {/* buttons */}
+          {otp ? (
+            <button
+              className="bg-[#ca1854e7] w-[100px] cursor-pointer rounded-lg transition-all duration-300 p-1 hover:bg-[#e91c60f8] hover:text-white disabled:bg-gray-500 disabled:cursor-not-allowed"
+              type="submit"
+              disabled={!signUpFormik.isValid}
+            >
+              Register
+            </button>
+          ) : (
+            <button
+              className="bg-[#ca1854e7] flex justify-center w-[100px] cursor-pointer rounded-lg transition-all duration-300 p-1 hover:bg-[#e91c60f8] hover:text-white disabled:bg-gray-500 disabled:cursor-not-allowed"
+              onClick={verifyHandler}
+              disabled={!emailEntered}
+            >
+              {isLoading ? <Loading w={24} h={20} /> : "Verify"}
+            </button>
+          )}
+        </div>
+      </form>
+    </section>
+  );
+}
---
+++ src/app/auth/page.js
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import SigninForm from "./SigninForm";
+import SignupForm from "./SignupForm";
+
+export default function AuthPage() {
+  const [move, setMove] = useState(false);
+  const [otp, setOtp] = useState('');
+
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   if (user) router.push("/");
+  // }, [user]);
+
+  return (
+    <main className="bg-signin bg-center bg-no-repeat bg-cover relative h-[50vh] mb-[480px] before:content-[''] before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-hero_before max-[900px]:mb-[590px]">
+      <div className="flex justify-center items-center min-h-[100vh] transition-all duration-300 relative z-40 max-[900px]:max-w-[500px] max-[900px]:h-[650px] max-[900px]:flex max-[900px]:items-center max-[900px]:justify-center max-[900px]:mt-[-40px]">
+        <div className="relative w-[800px] h-[500px] m-[20px]">
+          <div
+            className={`absolute top-[40px] w-full h-[420px] max-[900px]:h-[600px] flex rounded-[15px] justify-center items-center bg-[#0e253080] transition-all duration-500 shadow-[0_15px_45px_#ffffff1f] ${
+              move && "bg-[#33101080]"
+            }`}
+          >
+            <div className="relative w-[50%] h-full flex justify-center items-center flex-col max-[900px]:absolute max-[900px]:w-full max-[900px]:h-[150px] max-[900px]:top-0 ">
+              <h2 className="text-[1.2em] font-medium mb-[12px]">Already Have an Account ?</h2>
+              <button
+                className="px-[10px] py-1 bg-[#bebebeda] text-gray-800 font-semibold rounded-md transition-all duration-300 hover:bg-[#e6e6e6da]"
+                onClick={() => setMove(!move)}
+              >
+                Sign in
+              </button>
+            </div>
+            <div className="relative w-[50%] h-full flex justify-center items-center flex-col max-[900px]:absolute max-[900px]:w-full max-[900px]:h-[150px] max-[900px]:bottom-0">
+              <h2 className="text-[1.2em] font-medium mb-[12px]">Don't Have an Account ?</h2>
+              <button
+                className="px-[10px] py-1 bg-[#bebebeda] text-gray-800 font-semibold rounded-md transition-all duration-300 hover:bg-[#e6e6e6da]"
+                onClick={() => setMove(!move)}
+              >
+                Sign up
+              </button>
+            </div>
+          </div>
+
+          <div
+            className={`absolute top-0 left-0 w-[50%] max-[900px]:w-full ${
+              move ? "h-[650px]" : "h-full"
+            } ${otp == "forgot_password" && "h-[520px]"} ${
+              otp == "signup" && "h-[740px]"
+            } bg-[#1a1a1a] z-50 flex justify-center items-center shadow-[0_4px_45px_#ffffff1a] transition-all duration-[0.4s] overflow-hidden rounded-[15px] ${
+              move && "left-[50%] max-[900px]:top-[35%] max-[900px]:left-0"
+            }`}
+          >
+            {/* Signin */}
+            <SigninForm move={move} otp={otp} setOtp={setOtp}/>
+
+            {/* sign up */}
+            <SignupForm move={move} otp={otp} setOtp={setOtp} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
